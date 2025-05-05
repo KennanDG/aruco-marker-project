1a346cff4764b965bc91de395cc990d7c07b4bb2
Kennan Gauthier <KDGauthier@student.fullsail.edu>
2025-05-05 19:20:35 -0400

    Refactored detection logic into a single function for modularity. Final commit update.

--- main.py
+++ main.py
@@ -26,21 +26,17 @@ dict_6x6 = aruco.getPredefinedDictionary(aruco.DICT_6X6_250)
 
 parameters = aruco.DetectorParameters()
 
+# Spiderman 3D model
 spider_man = {
     "body": pywavefront.Wavefront("ar_models/Spiderman_Neversoft.obj", collect_faces=True),
     "eye_1": pywavefront.Wavefront("ar_models/Eye1.obj", collect_faces=True),
     "eye_2": pywavefront.Wavefront("ar_models/Eye2.obj", collect_faces=True),
 }
 
-# Load Spiderman 3D model
-body = pywavefront.Wavefront("ar_models/Spiderman_Neversoft.obj", collect_faces=True)
-eye_1 = pywavefront.Wavefront("ar_models/Eye1.obj", collect_faces=True)
-eye_2 = pywavefront.Wavefront("ar_models/Eye2.obj", collect_faces=True)
-
 
 # Pose estimation smoothing
-prev_rvec_4x4 = None
-prev_tvec_4x4 = None
+prev_rvec = None
+prev_tvec = None
 
 prev_rvec_5x5 = None
 prev_tvec_5x5 = None
@@ -110,24 +106,6 @@ def draw_camera_background(frame):
     glDeleteTextures([int(tex_id)])
 
 
-# Draws OpenGL axes in the pygame window
-def axes():
-    glBegin(GL_LINES)
-    # X - Red
-    glColor3f(1, 0, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0.1, 0, 0)
-    # Y - Green
-    glColor3f(0, 1, 0)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 0.1, 0)
-    # Z - Blue
-    glColor3f(0, 0, 1)
-    glVertex3f(0, 0, 0)
-    glVertex3f(0, 0, 0.1)
-    glEnd()
-
-
 # Draws 3D model in the pygame window
 def render(rvec, tvec, frame):
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Clears window
@@ -135,10 +113,6 @@ def render(rvec, tvec, frame):
     
     # resets current matrix as an identity matrix
     glLoadIdentity() 
-
-    # Debugging
-    print("tvec:", tvec.flatten())
-    print("rvec:", rvec.flatten())
 
 
     ##### Convert OpenCV camera coordinates to OpenGL world coordinates ####
@@ -152,8 +126,6 @@ def render(rvec, tvec, frame):
     # Last column is the translation vector
     view_matrix[:3, 3] = tvec.flatten() 
 
-    # Debugging
-    print("View Matrix:\n", view_matrix.flatten())
 
     # invert matrix to match OpenGL's expected world view
     view_matrix = np.linalg.inv(view_matrix)
@@ -170,18 +142,52 @@ def render(rvec, tvec, frame):
 
     glLoadMatrixf(view_matrix.T) # Load transposed matrix to OpenGL
 
-    # Render Spider-Man 3D model
-    glScalef(0.1, 0.1, 0.1)
-    # axes()
 
-    pywavefront.visualization.draw(eye_1)
-    pywavefront.visualization.draw(eye_2)
-    pywavefront.visualization.draw(body)
+    ####### Render Spider-Man 3D model #########
+
+    glScalef(0.1, 0.1, 0.1) # Scale model to appropriate size
+        
+    for i, obj in enumerate(spider_man.values()):
+        if i != 0:
+            glPushMatrix()
+            glTranslatef(0, 0, 0.08)  # Bring forward along Z axis
+            pywavefront.visualization.draw(obj) # Loads the eyes
+            glPopMatrix()
+        else:
+            pywavefront.visualization.draw(obj) # Loads the body
+
+
+def draw(frame, corners, marker_id):
+    aruco.drawDetectedMarkers(frame, corners, marker_id)
+            
+    # Calculate Pose estimation
+    rvec, tvec, _ = aruco.estimatePoseSingleMarkers(
+        corners,
+        aruco_marker_length, 
+        camera_matrix, 
+        dist_coeffs)            
     
-    # for obj in spider_man.values():
-    #     pywavefront.visualization.draw(obj)
+    # Ensures only a single rotation & translation vector are used
+    # for rendering
+    rvec = rvec[0]
+    tvec = tvec[0]
 
 
+    # Exponential moving average (EMA) to smooth flickering
+    global prev_rvec, prev_tvec
+    if prev_rvec is not None:
+        prev_rvec = alpha * prev_rvec + (1 - alpha) * rvec
+        prev_tvec = alpha * prev_tvec + (1 - alpha) * tvec
+    else:
+        prev_rvec = rvec
+        prev_tvec = tvec
+
+    render(prev_rvec, prev_tvec, frame)
+
+    pygame.display.flip() # updates entire frame
+    
+    # Display axes
+    cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec, tvec, 0.03)
 
 
 def main():
@@ -222,84 +228,35 @@ def main():
 
 
         # 4x4 detector
-        corners_4x4, ids_4x4, rejected_4x4 = detector_4x4.detectMarkers(grayScale)
+        corners_4x4, ids_4x4, _ = detector_4x4.detectMarkers(grayScale)
 
         # 5x5 detector
-        corners_5x5, ids_5x5, rejected_5x5 = detector_5x5.detectMarkers(grayScale)
+        corners_5x5, ids_5x5, _ = detector_5x5.detectMarkers(grayScale)
 
         # 6x6 detector
-        corners_6x6, ids_6x6, rejected_6x6 = detector_6x6.detectMarkers(grayScale)
+        corners_6x6, ids_6x6, _ = detector_6x6.detectMarkers(grayScale)
 
         
         ###### Display detected ArUco markers ########
 
         if ids_4x4 is not None:
-            aruco.drawDetectedMarkers(frame, corners_4x4, ids_4x4)
-            
-            # Calculate Pose estimation
-            rvec_4x4, tvec_4x4, objPoints_4x4 = aruco.estimatePoseSingleMarkers(
-                corners_4x4,
-                aruco_marker_length, 
-                camera_matrix, 
-                dist_coeffs)            
-            
-            # Ensures only a single rotation & translation vector are used
-            # for rendering
-            rvec_4x4 = rvec_4x4[0]
-            tvec_4x4 = tvec_4x4[0]
-
-
-            # Exponential moving average (EMA) to smooth flickering
-            global prev_rvec_4x4, prev_tvec_5x5
-            if prev_rvec_4x4 is not None:
-                prev_rvec_4x4 = alpha * prev_rvec_4x4 + (1 - alpha) * rvec_4x4
-                prev_tvec_4x4 = alpha * prev_tvec_4x4 + (1 - alpha) * tvec_4x4
-            else:
-                prev_rvec_4x4 = rvec_4x4
-                prev_tvec_4x4 = tvec_4x4
-
-            render(prev_rvec_4x4, prev_tvec_4x4, frame)
-
-            pygame.display.flip() # updates entire frame
-            
-            # Display axes
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec_4x4[0], tvec_4x4[0], 0.03)
+            draw(frame, corners_4x4, ids_4x4)
 
         if ids_5x5 is not None:
-            aruco.drawDetectedMarkers(frame, corners_5x5, ids_5x5)
-
-            # Calculate Pose estimation
-            rvec_5x5, tvec_5x5, objPoints_5x5 = aruco.estimatePoseSingleMarkers(
-                corners_5x5,
-                aruco_marker_length, 
-                camera_matrix, 
-                dist_coeffs)
-            
-            # Display axes
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec_5x5, tvec_5x5, 0.03)
+            draw(frame, corners_5x5, ids_5x5)
 
         if ids_6x6 is not None:
-            aruco.drawDetectedMarkers(frame, corners_6x6, ids_6x6)
-
-            # Calculate Pose estimation
-            rvec_6x6, tvec_6x6, objPoints_6x6 = aruco.estimatePoseSingleMarkers(
-                corners_6x6,
-                aruco_marker_length, 
-                camera_matrix, 
-                dist_coeffs)
-            
-            # Display axes
-            cv2.drawFrameAxes(frame, camera_matrix, dist_coeffs, rvec_6x6, tvec_6x6, 0.03)
+            draw(frame, corners_6x6, ids_6x6)
             
         cv2.imshow('Aruco Marker Detection', frame)
 
         # Press 'q' to quit
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            pygame.quit()
             break
 
     
     cap.release()
-    pygame.quit()
     cv2.destroyAllWindows()
 
         
